b2c7380fd5a397bff5e214ddd8ae90423d7c7017
Peter Daniel Nygaard <peter.kodehode@gmail.com>
2022-03-25 19:10:53 +0100

    More mobile responsive design

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -12,14 +12,14 @@ function AboutMe() {
 
           <img className="aboutgrey" src={MyFace} alt="Peter" />
 
-          <p>
+          <p><br />
             Hei! Jeg heter Peter og jeg er en relativt fersk spiller i
             utvikler-gamet! På Kodehode lærer jeg meg å utvikle nettsider, og er
             lysten på å tilegne meg enda mer kunnskap innenfor feltet. Jeg er
             ingen trollmann enda, men jeg går på en måte på Kode-Hogwarts, så
             det er godt mulig jeg blir det til slutt.
           </p>
-          <p>
+          <p><br />
             Jeg har jobbet en del med soloprosjekter der jeg har fulgt et gitt
             design, men har også jobbet med egne design eller idèer. Jeg har
             også jobbet på et større gruppeprosjekt så langt. Alt dette innenfor
@@ -28,21 +28,29 @@ function AboutMe() {
             JavaScript. Har også nylig blitt mer kjent med Node.js, React og
             APIer.
           </p>
-          <p>
+
+          <p><br />
             Her er litt info angående min erfaring fra andre muligens relevante
             felt. Jeg har fra en tidlig alder vært interessert i teknologi,
             gaming og data og har da anskaffet meg litt forskjellig erfaring fra
-            denne interessen. Jeg har alltid vært den som har måttet hjelpe
+            disse interessen. Jeg har alltid vært den som har måttet hjelpe
             familiemedlemmer med tekniske problemer, til min noen ganger store
-            ergelse. Var tidlig interessert i modding av spill, og lærte meg i
+            ergelse.
+          </p><br />
+          <p><br />Var tidlig interessert i modding av spill, og lærte meg i
             mine yngre år det å programmere litt i Java i sammenheng med at jeg
             laget meg en privatserver innenfor spillet RuneScape, som jeg da
             spilte en del. Har også litt smått med erfaring innenfor Photoshop
             jeg har brukt dette en del til å tulle med venner gjennom memes og
             interne spøker. Jeg har forsåvidt litt innsikt i nettverk ettersom
             jeg var en hyppig LAN deltaker og har tidligere hostet servere
-            innenfor forskjellige spill også. Utenom alt dette, så sier mamma at
-            jeg er en snill og kjekk gutt.
+            innenfor forskjellige spill også.
+          </p>
+          <p><br />
+            Gamer, skater, ser en del på film og hører mye på musikk.
+            Bruker også mye tid med venner og min datter på syv år.
+            Har også noen rare marsvin.
+            Utenom alt dette, så sier mamma at jeg er en snill og kjekk gutt.
           </p>
         </div>
         <div className="filler-image">
